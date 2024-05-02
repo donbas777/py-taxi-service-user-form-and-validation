2291aa8582245fd4599eea069d5caeb97c5175c3
donbas777 <renary34@gmail.com>
2024-05-02 22:52:27 +0300

    'Solution'

--- taxi/views.py
+++ taxi/views.py
@@ -4,8 +4,8 @@ from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from .forms import DriverCreationForm, DriverLicenseUpdateForm
-from .models import Driver, Car, Manufacturer
+from taxi.forms import DriverCreationForm, DriverLicenseUpdateForm
+from taxi.models import Driver, Car, Manufacturer
 
 
 @login_required
